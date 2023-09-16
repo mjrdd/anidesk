ad5047557882c0d76fa02f7d9cc925602c255e98
mjrdd <enigmaticjayar@gmail.com>
2023-09-16 15:14:18 +0800

    Init app layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { AppShell } from "@skeletonlabs/skeleton";
+  import AppRail from "./AppRail.svelte";
+</script>
+
+<AppShell>
+  <svelte:fragment slot="sidebarLeft">
+    <AppRail />
+  </svelte:fragment>
+
+  <slot />
+</AppShell>
---
+++ src/routes/(app)/AppRail.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { page } from "$app/stores";
+  import { AppRail, AppRailAnchor } from "@skeletonlabs/skeleton";
+
+  import Icon from "@iconify/svelte/offline";
+  import iconLibrary from "@iconify-icons/material-symbols/library-books-outline";
+</script>
+
+<AppRail>
+  <AppRailAnchor href="/" selected={$page.url.pathname === "/"} title="Library">
+    <svelte:fragment slot="lead">
+      <Icon icon={iconLibrary} width="24" />
+    </svelte:fragment>
+    <span>Library</span>
+  </AppRailAnchor>
+</AppRail>
